6e9581abb8d2ed83df7ede265e9b2d2e914bc200
Argyn <alauddin.aybat11@gmail.com>
2025-06-25 12:49:58 +0500

    Remove venv from repo and ignore it

--- diplom/settings.py
+++ diplom/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://django-proj-pp6x.onrender.com/"
+    "https://django-proj-pp6x.onrender.com"
 ]
 
 SECRET_KEY = 'django-insecure-4qj$w8g49d1b39j0#p4xvsb(qtdsnwf4c=&vu*j783r1pns+9l'
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,6 +84,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
